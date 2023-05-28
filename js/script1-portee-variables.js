8abea8757dcc3ddd72500af51bf37df72d85a026
Camille <cambordes001@gmail.com>
2023-05-28 13:59:30 +0200

    premiers vrai cours ça se passe bien

---
+++ js/script1-portee-variables.js
@@ -0,0 +1,9 @@
+console.log("############################################");
+
+let aa = 18;
+let suffixe = "ah";
+if(aa){
+    const suffixe = "oui";
+    console.log(suffixe);
+}
+console.log(suffixe);
