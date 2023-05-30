80b69340ead7fa428c3fa2528027110a3d39a5bd
Camille <cambordes001@gmail.com>
2023-05-30 18:31:43 +0200

    svg

--- js/script2-fonctions.js
+++ js/script2-fonctions.js
@@ -38,11 +38,57 @@ aa.fullname();
 
 const myStr = "bonjour";
 
-console.log(myStr.length)
-console.log(myStr.toUpperCase())
+console.log(myStr.length);
+console.log(myStr.toUpperCase());
 
 
 const aaa = 'aaa', bbb="bbb";
 
-console.log(aaa, bbb, "il était une fois", 43)
+console.log(aaa, bbb, "il était une fois", 43);
+
+const myFunction2 = (para1, para2) => {
+    console.log(para1, para2, this);
+}
+
+myFunction2.call(2, 4, 5);
+
+
+const somme = (a, b) => a+b;
+
+console.log(somme(3,4));
+
+printf("#############Exercice#################");
+
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * (max + 1));
+}
+const solution = getRandomInt(10);
+
+printf("solution : " + solution)
+
+function demanderNb(n){
+    return n===solution;
+}
+
+function guess(){
+    return demanderNb(prompt("Entrez le nombre de votre choix : ") * 1);
+}
+
+
+
+let cpt = 3;
+while(cpt >= 1){
+    console.log("Devinez le nombre (attention, il ne vous reste plus que", cpt, "essais)");
+    if (guess()){
+        console.log("Bravo");
+        break;
+    }
+    else if (cpt == 1){
+        console.log("Vous avez perdu")
+        break;
+    }
+    cpt--;
+}
+ 
 
