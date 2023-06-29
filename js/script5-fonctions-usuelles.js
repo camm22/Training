332c077324da8331076af10cf0576c1336d77ab1
Camille <cambordes001@gmail.com>
2023-06-29 19:17:43 +0200

    paus

---
+++ js/script5-fonctions-usuelles.js
@@ -0,0 +1,21 @@
+//fonctinos sur tableau
+
+const notes = [12, 17, 18, 8, 9];
+
+console.log(notes.at(-1));
+
+const goodNotes = notes.filter(note => note >= 10);  
+console.log(goodNotes);
+
+const found = notes.find(note => note > 15);  //on a aussi findLast
+console.log(found);
+
+const indexFound = notes.findIndex(note => note > 15); //et findLastIndex
+console.log(indexFound);
+
+notes.forEach((note,index) => {
+    console.log(note,index);
+})
+
+notes.forEach((note, index) => console.log(note, index));
+
